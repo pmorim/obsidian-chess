2287c11240df86368c78d8f908015266ae126bec
Pedro Morim <1180798@isep.ipp.pt>
2021-10-04 18:27:14 +0100

    Added calculation of the piece's file name

--- src/svelte/Piece.svelte
+++ src/svelte/Piece.svelte
@@ -3,9 +3,11 @@
 
   const pathToPieces = ('assets/piece-sets');
   const pieceSet = 'cburnett';
+
+  const isUpperCase = (char: string) => /^[A-Z]*$/.test(char)
+  $: pieceColor = isUpperCase(pieceCode) ? 'w' : 'b'
+  $: pieceFile = `${pieceColor}${pieceCode.toUpperCase()}.svg`
+  $: piecePath = `${pathToPieces}/${pieceSet}/${pieceFile}`
 </script>
 
-<img
-  src='{pathToPieces}/{pieceSet}/{pieceCode}.svg'
-  alt={pieceCode}
-/>
+<img src={piecePath} alt={pieceCode} />
