38c3ffed7bbe74c7aa8426f06c2adf7dd94cdf8b
Pedro Morim <1180798@isep.ipp.pt>
2021-10-05 21:32:09 +0100

    Added basic support for lastMove and check overlay

--- src/svelte/Square.svelte
+++ src/svelte/Square.svelte
@@ -2,17 +2,29 @@
   import Piece from './Piece.svelte';
   import { settings } from '../settings';
 
-  export let isLightSquare: boolean;
-  export let pieceCode: string;
+  export let pieceCode = '';
+  export let isLight = false;
+  export let lastMove = false;
+  export let check = false;
 
   $: boardSize = parseInt($settings.boardSize, 10);
   $: size = boardSize / 8;
+
+  $: light = isLight;
+  $: dark = !isLight;
 </script>
 
 <div
-  class="square {$settings.boardTheme} {isLightSquare ? 'light' : 'dark'}"
   style="--size: {size}px;"
+  class="square {$settings.boardTheme}"
+  class:light
+  class:dark
+  class:lastMove
+  class:check
 >
+  {#if lastMove || check}
+    <div class="overlay" class:lastMove class:check />
+  {/if}
   {#if pieceCode}
     <Piece fenCode={pieceCode} />
   {/if}
@@ -23,34 +35,67 @@
   .blue {
     --light: #dee3e6;
     --dark: #8ca2ad;
+    --lastMove-light: #c3d887;
+    --lastMove-dark: #92b166;
   }
   .brown {
     --light: #f0d9b5;
     --dark: #b58863;
+    --lastMove-light: #cdd26a;
+    --lastMove-dark: #aaa23a;
   }
   .green {
     --light: #ffffdd;
     --dark: #86a666;
+    --lastMove-light: #96d6d4;
+    --lastMove-dark: #4fa28e;
   }
   .ic {
     --light: #ececec;
     --dark: #c1c18e;
+    --lastMove-light: #cbdd8b;
+    --lastMove-dark: #80883e;
   }
   .purple {
     --light: #9f90b0;
     --dark: #7d4a8d;
+    --lastMove-light: #9ea768;
+    --lastMove-dark: #8a7e53;
   }
 
   /* Light or Dark square */
   .light {
     --color: var(--light);
+    --lastMove-color: var(--lastMove-light);
   }
   .dark {
     --color: var(--dark);
+    --lastMove-color: var(--lastMove-dark);
+  }
+
+  /* Color overlays */
+  .lastMove {
+    --overlay-color: var(--lastMove-color) !important;
+  }
+  .check {
+    --overlay-color: #f00 !important;
+  }
+  .square > .overlay {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+
+    --overlay-color: #fff0; /* transparent */
+    background-color: var(--overlay-color);
   }
 
   /* The square */
   .square {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
     width: var(--size);
     height: var(--size);
     background-color: var(--color);
