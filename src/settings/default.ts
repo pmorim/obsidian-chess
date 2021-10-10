e2507fee3893f47dabb76cd3db2943203e6d5c6f
Pedro Morim <1180798@isep.ipp.pt>
2021-10-10 14:10:21 +0100

    Updated default settings' type

--- src/settings/default.ts
+++ src/settings/default.ts
@@ -1,6 +1,6 @@
 import type { Settings } from './types';
 
-export const DEFAULT_SETTINGS: Settings = {
+export const DEFAULT_SETTINGS: Partial<Settings> = {
   boardTheme: 'blue',
   pieceSet: 'cburnett',
   boardSize: '500',
