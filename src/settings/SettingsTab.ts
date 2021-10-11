6335f0a3749035f4194f7b0761dde04d7470d5b4
Pedro Morim <1180798@isep.ipp.pt>
2021-10-11 01:36:12 +0100

    Turned settings into dropdown menus

--- src/settings/SettingsTab.ts
+++ src/settings/SettingsTab.ts
@@ -1,52 +1,96 @@
-import { App, PluginSettingTab, Setting, TextComponent } from 'obsidian';
+import {
+  App,
+  PluginSettingTab,
+  Setting,
+  TextComponent,
+  DropdownComponent,
+} from 'obsidian';
 import lodash from 'lodash';
 
-import { settings } from './store';
-import type { Settings } from './types';
+import { settings as settingsStore } from './store';
+import { Settings, BOARD_THEMES_RECORD, PIECE_SETS_RECORD } from './types';
 
 export class SettingsTab extends PluginSettingTab {
   plugin: any;
+  settings: Settings;
 
   constructor(app: App, plugin: any) {
     super(app, plugin);
     this.plugin = plugin;
-    settings.set(this.plugin.settings);
+
+    settingsStore.set(this.plugin.settings);
+    settingsStore.subscribe((value) => (this.settings = <Settings>value));
   }
 
-  createSetting(name: string, desc: string) {
+  display() {
+    this.containerEl.empty();
+    this.settingsTitle('Obsidian Chess');
+
+    this.generalSettings();
+    this.customThemes();
+  }
+
+  generalSettings() {
+    this.sectionTitle('General');
+
+    this.textSetting(
+      'Board Size',
+      'The width and height of the board, in pixels'
+    );
+    this.dropdownSetting(
+      'Piece Set',
+      'The style of the pieces',
+      PIECE_SETS_RECORD
+    );
+    this.dropdownSetting(
+      'Board Theme',
+      'The colors of the board tiles',
+      BOARD_THEMES_RECORD
+    );
+  }
+
+  customThemes() {
+    this.sectionTitle('Custom Themes');
+    this.containerEl.createEl('p', { text: 'Coming soon...' });
+  }
+
+  textSetting(name: string, desc: string) {
     const key = lodash.camelCase(name);
-
-    let store: Settings;
-    settings.subscribe((value: Settings) => (store = value));
-
     new Setting(this.containerEl)
       .setName(name)
       .setDesc(desc)
       .addText((text: TextComponent) =>
-        text.setValue(store[key]).onChange(async (value: string) => {
-          // Svelte store
-          settings.update((settings) => ({ ...settings, [key]: value }));
-
-          // Obsidian
-          this.plugin.settings[key] = value;
-          await this.plugin.saveSettings();
-        })
+        text.setValue(this.settings[key]).onChange(this.onSettingChange(key))
       );
   }
 
-  display(): void {
-    this.containerEl.empty();
-    this.containerEl.createEl('h2', { text: 'Obsidian Chess' });
+  dropdownSetting(name: string, desc: string, options: Record<string, string>) {
+    const key = lodash.camelCase(name);
+    new Setting(this.containerEl)
+      .setName(name)
+      .setDesc(desc)
+      .addDropdown((dropdown: DropdownComponent) => {
+        dropdown
+          .addOptions(options)
+          .setValue(this.settings[key])
+          .onChange(this.onSettingChange(key));
+      });
+  }
 
-    this.containerEl.createEl('h3', { text: 'General' });
-    this.createSetting('Board Theme', 'The theme of the board tiles');
-    this.createSetting('Piece Set', 'The style of the pieces');
-    this.createSetting(
-      'Board Size',
-      'The width and height of the board, in pixels'
-    );
+  onSettingChange(key: string) {
+    return async (value: string) => {
+      settingsStore.update((settings) => ({ ...settings, [key]: value }));
 
-    this.containerEl.createEl('h3', { text: 'Advanced' });
-    this.containerEl.createEl('p', { text: 'Coming soon...' });
+      this.plugin.settings[key] = value;
+      await this.plugin.saveSettings();
+    };
+  }
+
+  settingsTitle(text: string) {
+    this.containerEl.createEl('h2', { text });
+  }
+
+  sectionTitle(text: string) {
+    this.containerEl.createEl('h3', { text });
   }
 }
--- src/settings/types.ts
+++ src/settings/types.ts
@@ -1,6 +1,62 @@
 export interface Settings {
-  [key: string]: string;
-  boardTheme: string;
-  pieceSet: string;
+  /**
+   * The colors of the board tiles
+   */
+  boardTheme: BoardTheme;
+  /**
+   * The style of the pieces
+   */
+  pieceSet: PieceSet;
+  /**
+   * The width and height of the board, in pixels
+   */
   boardSize: string;
+  /**
+   * Workaround for linting purposes
+   */
+  [key: string]: string;
 }
+
+function getRecord(arr: readonly string[]) {
+  return <Record<string, string>>(
+    Object.fromEntries(arr.map((val: string) => [val, val]))
+  );
+}
+
+export const BOARD_THEMES = ['blue', 'brown', 'green', 'ic', 'purple'] as const;
+export const BOARD_THEMES_RECORD = getRecord(BOARD_THEMES);
+export type BoardTheme = typeof BOARD_THEMES[number];
+
+// TODO: generate this array by looking at the assets folder's names
+export const PIECE_SETS = [
+  'alpha',
+  'california',
+  'cardinal',
+  'cburnett',
+  'chess7',
+  'chessnut',
+  'companion',
+  'dubrovny',
+  'fantasy',
+  'fresca',
+  'gioco',
+  'governor',
+  'horsey',
+  'icpieces',
+  'kosal',
+  'leipzig',
+  'letter',
+  'libra',
+  'maestro',
+  'merida',
+  'pirouette',
+  'pixel',
+  'reillycraig',
+  'riohacha',
+  'shapes',
+  'spatial',
+  'staunty',
+  'tatiana',
+] as const;
+export const PIECE_SETS_RECORD = getRecord(PIECE_SETS);
+export type PieceSet = typeof PIECE_SETS[number];
