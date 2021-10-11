3e77c86c274350ac676246058d6f4ad0199108c5
Pedro Morim <1180798@isep.ipp.pt>
2021-10-11 22:57:18 +0100

    Initial Storybook config

---
+++ src/stories/Square.stories.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import Square from '../svelte/Square.svelte';
+</script>
+
+<Meta title="Square" component={Square} />
+
+<Template let:args>
+  <Square {...args} />
+</Template>
+
+<Story
+  name="Light Square"
+  args={{
+    variant: 'light',
+  }}
+/>
+
+<Story
+  name="Dark Square"
+  args={{
+    variant: 'dark',
+  }}
+/>
